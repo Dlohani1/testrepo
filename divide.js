dd3b95e8284cee9eb4847d2b9c279d1f6027614a
Deepak <dlohani1@gmail.com>
2022-07-26 20:38:55 +0530

    division functionality done

--- divide.js
+++ divide.js
@@ -1,4 +1,6 @@
 function divide() {
-
-
+var firstno = document.getElementById("firstno").value
+var secondno = document.getElementById("secondno").value
+result = firstno/secondno
+document.getElementById("result").value= result
 }
