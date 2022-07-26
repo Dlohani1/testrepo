faf508147906cfcf9906b648435f849eb9b04b2c
Deepak <dlohani1@gmail.com>
2022-07-26 21:08:29 +0530

    Revert "division functionality done"
    This reverts commit dd3b95e8284cee9eb4847d2b9c279d1f6027614a.

--- divide.js
+++ divide.js
@@ -1,6 +1,4 @@
 function divide() {
-var firstno = document.getElementById("firstno").value
-var secondno = document.getElementById("secondno").value
-result = firstno/secondno
-document.getElementById("result").value= result
+
+
 }
