33efecf276e99160369d4caa337c990efe378d04
Deepak <dlohani1@gmail.com>
2022-07-27 20:34:13 +0530

    addition file modified

---
+++ addition.js
@@ -0,0 +1,9 @@
+function add() {
+//this is addition function
+var firstno = document.getElementById("firstno").value
+var secondno = document.getElementById("secondno").value
+var result = parseInt(firstno) + parseInt(secondno)
+
+document.getElementById("result").value=result
+
+}
